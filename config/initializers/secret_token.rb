fcb20eadd86eca45687204f460f4fe702afb0a64
Daniel Petrarca <petrarca.11@osu.edu>
2013-06-20 19:03:06 -0400

    Basic Functionality

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dp::Application.config.secret_token = '051565183f8eefd385758847f2a8f0377672f88eb62eddfeedc374c3835888b92a832f56fd02e5c9d92b4870bc7d8f538b5fba75927d4efcf6e801f0540c37aa'
